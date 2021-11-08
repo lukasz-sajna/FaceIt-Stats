fefa13c41cff81961227cd9d70c179eabd77a929
Łukasz Sajna <lukasz.sajna@gmail.com>
2021-11-08 17:35:52 +0100

    Auto migration

--- FaceItStats.Api/Const.cs
+++ FaceItStats.Api/Const.cs
@@ -4,5 +4,6 @@
     {
         public static readonly string DefaultCorsPolicy = "DefaultCorsPolicy";
         public static readonly string SignalRHubsPathRoot = "/hubs";
+        public static readonly string ConnectionString = @"DataSource=persistence/db/FaceItDb.db;";
     }
 }
---
+++ FaceItStats.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using FaceItStats.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace FaceItStats.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FaceIt Stats API",
+                    Version = "v1"
+                });
+            });
+
+            return services;
+        }
+
+        public static void AddDb(this IServiceCollection serviceCollection, string connectionString)
+        {
+            serviceCollection.AddDbContext<FaceitDbContext>(options => options.UseSqlite(connectionString));
+        }
+
+        public static void MigrateStatisticsDb(IServiceProvider serviceProvider)
+        {
+            using var dbContext = serviceProvider.GetService<FaceitDbContext>();
+            dbContext.Database.Migrate();
+        }
+    }
+}
