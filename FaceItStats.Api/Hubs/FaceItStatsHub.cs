3cc8a81011a4bc35d5ff73afffd9a020072a05d9
Łukasz Sajna <lukasz.sajna@gmail.com>
2021-11-10 02:28:48 +0100

    Added deserializer

---
+++ FaceItStats.Api/Hubs/FaceItStatsHub.cs
@@ -0,0 +1,22 @@
+﻿using FaceItStats.Api.Client;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace FaceItStats.Api.Hubs
+{
+    public class FaceItStatsHub : Hub, IFaceItStatsHub
+    {
+        private readonly FaceItStatsClient _faceItClient;
+        public FaceItStatsHub()
+        {
+            _faceItClient = new FaceItStatsClient();
+        }
+
+        public async Task NotifyFaceItStatsChanged()
+        {
+            var stats = await _faceItClient.GetStatsForNickname("luciusxsein");
+
+            await Clients.All.SendAsync("StatsRefreshed", stats);
+        }
+    }
+}
